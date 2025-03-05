eabd6719845cf94f3c4ab7dd1b247e2e8faeef9a
Siang Huang <sianglifen@gmail.com>
2025-03-05 18:06:55 +0800

    Add random

---
+++ src/lib/utils.ts
@@ -0,0 +1,8 @@
+export function randomOnce(list: any[]): any {
+    return list[Math.floor(Math.random() * list.length)];
+}
+
+export function randomArray(list: any[], count?: number) {
+    count = count || list.length;
+    return list.sort(() => Math.random() - 0.5).slice(0, count);
+}
